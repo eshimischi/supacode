6d9275575fc2da28c4cb5bb0fe08e55e838620e0
khoi <git@khoi.io>
2026-01-26 12:51:11 +0700

    Add backward-compatible decoder for GlobalSettings
    Migrate old settings without inAppNotificationsEnabled field

--- supacode/Features/Settings/Models/GlobalSettings.swift
+++ supacode/Features/Settings/Models/GlobalSettings.swift
@@ -10,4 +10,24 @@ nonisolated struct GlobalSettings: Codable, Equatable {
     updatesAutomaticallyDownloadUpdates: false,
     inAppNotificationsEnabled: true
   )
+
+  init(
+    appearanceMode: AppearanceMode,
+    updatesAutomaticallyCheckForUpdates: Bool,
+    updatesAutomaticallyDownloadUpdates: Bool,
+    inAppNotificationsEnabled: Bool
+  ) {
+    self.appearanceMode = appearanceMode
+    self.updatesAutomaticallyCheckForUpdates = updatesAutomaticallyCheckForUpdates
+    self.updatesAutomaticallyDownloadUpdates = updatesAutomaticallyDownloadUpdates
+    self.inAppNotificationsEnabled = inAppNotificationsEnabled
+  }
+
+  init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    appearanceMode = try container.decode(AppearanceMode.self, forKey: .appearanceMode)
+    updatesAutomaticallyCheckForUpdates = try container.decode(Bool.self, forKey: .updatesAutomaticallyCheckForUpdates)
+    updatesAutomaticallyDownloadUpdates = try container.decode(Bool.self, forKey: .updatesAutomaticallyDownloadUpdates)
+    inAppNotificationsEnabled = try container.decodeIfPresent(Bool.self, forKey: .inAppNotificationsEnabled) ?? Self.default.inAppNotificationsEnabled
+  }
 }
